fdbe964be1d92dbc560c3f645f84dd43338ed72f
rishavk1102 <rishavk1102@gmail.com>
2020-06-27 15:09:04 +0530

    Register users

--- code/user.py
+++ code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 
 class User:
@@ -41,3 +42,26 @@ class User:
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True,
+                        help='Username cannot be empty')
+    parser.add_argument('password', type=str, required=True,
+                        help='Password cannot be empty')
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'User registered sucessfully'}, 201
